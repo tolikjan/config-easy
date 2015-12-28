b60ae1902773aecd2186cf37f3cbdc7e87ca1bb6
tolikjan <tolikjan@gmail.com>
2015-12-28 10:24:32 +0200

    add hotfix for mysql secure instlation

--- config-ubuntu.sh
+++ config-ubuntu.sh
@@ -183,76 +183,8 @@ apt-get install mysql-server -y
 mysql_install_db
 # Run secure instalation for MySQL
 #echo "mysql-server mysql-server/root_password password "${mysql_root_password} | debconf-set-selections
-/usr/bin/mysql_secure_installation -y
-
-# TODO: fix problems with this shit below
-
-# Delete package expect when script is done
-# 0 - No; 
-# 1 - Yes.
-PURGE_EXPECT_WHEN_DONE=0
-#
-# Check the bash shell script is being run by root
-#
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 1>&2
-   exit 1
-fi
-#
-# Check input params
-#
-if [ -n "${1}" -a -z "${2}" ]; then
-    # Setup root password
-    CURRENT_MYSQL_PASSWORD=''
-    NEW_MYSQL_PASSWORD="${1}"
-elif [ -n "${1}" -a -n "${2}" ]; then
-    # Change existens root password
-    CURRENT_MYSQL_PASSWORD="${1}"
-    NEW_MYSQL_PASSWORD="${2}"
-else
-    echo "Usage:"
-    echo "  Setup mysql root password: ${0} 'your_new_root_password'"
-    echo "  Change mysql root password: ${0} 'your_old_root_password' 'your_new_root_password'"
-    exit 1
-fi
-#
-# Check is expect package installed
-#
-if [ $(dpkg-query -W -f='${Status}' expect 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
-    echo "Can't find expect. Trying install it..."
-    apt-get install expect -y
-fi
-SECURE_MYSQL=$(expect -c "
-set timeout 3
-spawn mysql_secure_installation
-expect \"Enter current password for root (enter for none):\"
-send \"$CURRENT_MYSQL_PASSWORD\r\"
-expect \"root password?\"
-send \"y\r\"
-expect \"New password:\"
-send \"$NEW_MYSQL_PASSWORD\r\"
-expect \"Re-enter new password:\"
-send \"$NEW_MYSQL_PASSWORD\r\"
-expect \"Remove anonymous users?\"
-send \"y\r\"
-expect \"Disallow root login remotely?\"
-send \"y\r\"
-expect \"Remove test database and access to it?\"
-send \"y\r\"
-expect \"Reload privilege tables now?\"
-send \"y\r\"
-expect eof
-")
-#
-# Execution mysql_secure_installation
-#
-echo "${SECURE_MYSQL}"
-if [ "${PURGE_EXPECT_WHEN_DONE}" -eq 1 ]; then
-    # Uninstalling expect package
-    apt-get purge expect -y
-fi
-
-
+echo -e "\ny\ny\n${mysql_root_password}\n${mysql_root_password}\ny\ny\ny\ny" | ./usr/bin/mysql_secure_installation
+# TODO: check this shit above
 # install PHP and php-packages
 apt-get install php5 curl php5-curl php5-fpm php5-mysql mysql-server php5-gd php5-mcrypt php5-xdebug php5-memcached php5-memcache php5-json -y
 # Change configuration for better security and convenience
@@ -303,6 +235,8 @@ cat > ${php_info_path} << EOF
 	phpinfo();
 	?>
 EOF
+
+
 
 
 
