2c20a50abd260ce21bfc97a31642956ece9cdce3
tolikjan <tolikjan@gmail.com>
2016-01-05 17:46:28 +0200

    fix a ton of code which related to nginx configuration

--- config-ubuntu.sh
+++ config-ubuntu.sh
@@ -4,18 +4,12 @@
 # Tested on Ubuntu 14.04.3 64x
 
 # Password for root user for ubuntu box http://sourceforge.net/projects/osboxes/files/vms/vbox/Ubuntu/14.04/14.04.3/Ubuntu_14.04.3-64bit.7z/download
-#root_pass="osboxes.org"
+root_pass="osboxes.org"
 
 # coloured variables
 red=`tput setaf 1`
 green=`tput setaf 2`
 reset=`tput sgr0`
-
-# mysql variables
-mysql_config_file="/etc/mysql/my.cnf"
-mysql_root_user="root"
-mysql_root_password="root"
-phpmyadmin_password="root"
 
 sleep 5
 #
@@ -170,18 +164,31 @@ echo ${green}...................................................................
 php_config_file_1="/etc/php5/fpm/php.ini"
 php_config_file_2="/etc/php5/cli/php.ini"
 php_config_file_3="/etc/php5/cgi/php.ini"
-# Server name
-server_name="local.host"
-# Nginx config files
+# site folder path
+site_path="/var/www/html"
+# www config
 www_conf="/etc/php5/fpm/pool.d/www.conf"
+# nginx config
 nginx_conf="/etc/nginx/nginx.conf"
+# default nginx config
 default_nginx_conf="/etc/nginx/sites-available/default"
 default_nginx_conf_link="/etc/nginx/sites-available/default"
-local_conf="/etc/nginx/sites-available/local.host"
-local_path="/usr/share/nginx/html/local.host"
-mime_types="/etc/nginx/mime.types"
-proxy_conf="/etc/nginx/proxy.conf"
-fastcgi_conf="/etc/nginx/fastcgi.conf"
+# additional config
+basic_settings="/etc/nginx/basic_settings"
+client_settings="/etc/nginx/client_settings"
+fastcgi_caches="/etc/nginx/fastcgi_caches"
+fastcgi_params="/etc/nginx/fastcgi_params"
+gzip_settings="/etc/nginx/gzip_settings"
+client_settings="/etc/nginx/client_settings"
+mime.types="/etc/nginx/mime.types"
+microcache_zone="/etc/nginx/microcache_zone"
+# phpmyadmin config
+phpmyadmin.conf="/usr/share/phpmyadmin/phpmyadmin.conf"
+phpmyadmin_root_password="root"
+# mysql variables
+mysql_config_file="/etc/mysql/my.cnf"
+mysql_root_user="root"
+mysql_root_password="root"
 #
 # Install nginx
 #
@@ -222,48 +229,354 @@ sed -i "s/listen =  127.0.0.1:9000/listen = /var/run/php5-fpm.sock/g" ${www_conf
 service nginx start
 service php5-fpm start
 # Remove default settings for nginx
-
-
 cp ${default_nginx_conf} ${default_nginx_conf}.backup
-mkdir ${conf}
-chmod 777 -R ${conf}
-rm -rf ${default_nginx_conf}
-rm -rf ${default_nginx_conf_link}
-
-# Configure nginx for ${server_name}
-cat > ${local_conf} << EOF
+# Configure nginx for http://localhost/
+cat > ${default_nginx_conf} << EOF
 server {
-    listen 80 default_server;
-    listen [::]:80 default_server ipv6only=on;
-    root ${local_path};
+    listen   80 default_server; ## listen for ipv4; 
+    listen   [::]:80 default_server ipv6only=on; ## listen for ipv6
+
+    root ${site_path};
     index index.php index.html index.htm;
-    server_name ${server_name};
+
+    # Make site accessible from http://localhost/ to all projects
+    server_name _;
+
     location / {
         # First attempt to serve request as file, then
         # as directory, then fall back to displaying a 404.
-        try_files \$uri \$uri/ =404;
-        # Uncomment to enable naxsi on this location
-        # include /etc/nginx/naxsi.rules
+        try_files \$uri \$uri/ /index.html;
+        expires max;
+        autoindex on;
     }
-    error_page 404 /404.html;
+
+     location ~* ^.+\\.(cur|js|jpe?g|ico|gif|png|svg|css|mp3|ogg|mpe?g|avi|zip|gz|bz2?|rar|swf)\$ {
+                expires 7d;
+                access_log off;
+                tcp_nodelay off;
+                open_file_cache max=3000 inactive=120s;
+                open_file_cache_valid 45s;
+                open_file_cache_min_uses 2;
+                open_file_cache_errors off;
+        }
+
+    # ImageCache
+    location ~ ^/imagecache/ {
+        try_files \$uri @rewrite;
+    }
+    
+    location @rewrite {
+                rewrite ^/(.*)\$ /index.php?q=\$1;
+        }
+    
+    ## Include phpmyadmin location here
+    include phpmyadmin.conf;
+    
+    location /doc/ {
+        alias /usr/share/doc/;
+        allow 127.0.0.1;
+        allow ::1;
+        deny all;
+    }
+
+    # redirect server error pages to the static page /50x.html
+    #
     error_page 500 502 503 504 /50x.html;
     location = /50x.html {
-        root /usr/share/nginx/html;
+        root errors/;
     }
-    location ~ \.php\\\$ {
-        try_files $uri =404;
-        fastcgi_split_path_info ^(.+\.php)(/.+)\$;
-        fastcgi_pass unix:/var/run/php5-fpm.sock;
+
+    # pass the PHP scripts to FastCGI server
+    #
+    location ~ \\.php\$ {
+    
+    #   # For Micro Caching
+    #   include microcache.conf;
+
+    #   # With php5-cgi alone:
+    #   fastcgi_pass 127.0.0.1:9000;
+    #   # With php5-fpm:
+        fastcgi_pass unix:/tmp/php5-fpm.sock;
         fastcgi_index index.php;
-        include fastcgi.conf;
+        fastcgi_param  SCRIPT_FILENAME  $document_root\$fastcgi_script_name;
+        include fastcgi_params;
+        include fastcgi_caches;
     }
+    
+    ## Disallow access to .git directory: return 404 as not to disclose information.
+    location ^~ /.git {
+        return 404;
+    }
+    
+    ## Support for favicon. Return an 1x1 transparent GIF if it doesn't exist.
+    location = /favicon.ico {
+        expires 30d;
+        try_files /favicon.ico @empty;
+    }
+
+    ## Return an in memory 1x1 transparent GIF.
+    location @empty {
+        expires 30d;
+        empty_gif;
+    }
+
+    ## Any other attempt to access PHP files returns a 404.
+    location ~* ^.+\\.php$ {
+        return 404;
+    }
+
 }
 EOF
-ln -s ${conf} /etc/nginx/sites-enabled/
+# Configure nginx.conf
+cat > ${nginx_conf} << EOF
+user www-data;
+master_process on;
+worker_processes 4;
+pid /var/run/nginx.pid;
+
+events {
+    worker_connections  1024;
+     multi_accept on;
+}
+
+http {
+    
+    ##
+    # Include Nginx Basic Settings
+    ##
+    
+    include basic_settings;
+    
+    ##
+    # Include Client Settings and Timeouts
+    ##
+    
+    include client_settings;
+
+    ##
+    # Include Nginx Logging Settings
+    ##
+
+    include log_settings;
+
+    ##
+    # Include Nginx Gzip Settings
+    ##
+
+    include gzip_settings;
+
+    ##
+    # Virtual Host Configs
+    ##
+    
+    include sites-enabled/*;
+    
+    ##
+    # Include Microcache Zone Settings
+    ##
+    
+    include microcache_zone;
+}
+EOF
+# Configure basic_settings
+cat > ${basic_settings} << EOF
+##
+# Nginx Basic Settings
+##
+
+sendfile on;
+tcp_nopush on;
+tcp_nodelay on;
+keepalive_timeout 10;
+types_hash_max_size 2048;
+# server_tokens on;
+server_tokens on;
+
+# server_names_hash_bucket_size 64;
+server_names_hash_bucket_size 64;
+
+# Mime types
+include mime.types;
+default_type application/octet-stream;
+EOF
+# Configure client_settings
+cat > ${client_settings} << EOF
+## Timeouts.
+client_body_timeout                 60;
+client_header_timeout               60;
+keepalive_timeout                10 10;
+send_timeout                        60;
+
+## Body Size
+client_max_body_size              128M;
+
+## Reset lingering timed out connections. Deflect DDoS.
+reset_timedout_connection           on;
+
+## Buffer Size
+client_header_buffer_size           1k;
+large_client_header_buffers       4 4k;
+EOF
+# Configure fastcgi_caches
+cat > ${fastcgi_caches} << EOF
+# Bypass cache if flag is set
+        fastcgi_no_cache            \$no_cache;
+        fastcgi_cache_bypass        \$no_cache;
+        fastcgi_cache               microcache;
+        fastcgi_cache_key           \$server_name|\$request_uri;
+        fastcgi_cache_valid         404 30m;
+        fastcgi_cache_valid         200 10s;
+        fastcgi_max_temp_file_size  1M;
+        fastcgi_cache_use_stale     updating;
+        fastcgi_pass_header         Set-Cookie;
+        fastcgi_pass_header         Cookie;
+        fastcgi_ignore_headers      Cache-Control Expires Set-Cookie;
+EOF
+# Configure fastcgi_params
+cat > ${fastcgi_params} << EOF
+fastcgi_param   QUERY_STRING        \$query_string;
+fastcgi_param   REQUEST_METHOD      \$request_method;
+fastcgi_param   CONTENT_TYPE        \$content_type;
+fastcgi_param   CONTENT_LENGTH      \$content_length;
+
+fastcgi_param   SCRIPT_FILENAME     \$request_filename;
+fastcgi_param   SCRIPT_NAME         \$fastcgi_script_name;
+fastcgi_param   REQUEST_URI         \$request_uri;
+fastcgi_param   DOCUMENT_URI        \$document_uri;
+fastcgi_param   DOCUMENT_ROOT       \$document_root;
+fastcgi_param   SERVER_PROTOCOL     \$server_protocol;
+
+fastcgi_param   GATEWAY_INTERFACE   CGI/1.1;
+fastcgi_param   SERVER_SOFTWARE     nginx/\$nginx_version;
+
+fastcgi_param   REMOTE_ADDR         \$remote_addr;
+fastcgi_param   REMOTE_PORT         \$remote_port;
+fastcgi_param   SERVER_ADDR         \$server_addr;
+fastcgi_param   SERVER_PORT         \$server_port;
+fastcgi_param   SERVER_NAME         \$server_name;
+
+fastcgi_param   HTTPS               \$https;
+
+# PHP only, required if PHP was built with --enable-force-cgi-redirect
+fastcgi_param   REDIRECT_STATUS     200;
+EOF
+# Configure gzip_settings
+cat > ${gzip_settings} << EOF
+##
+# Gzip Settings
+##
+gzip  on;
+gzip_buffers 16 8k;
+gzip_comp_level 5;
+gzip_http_version 1.1;
+gzip_min_length 10;
+gzip_types text/plain text/css image/png image/gif image/jpeg application/x-javascript text/xml application/xml application/xml+rss text/javascript image/x-icon;
+gzip_vary on;
+gzip_static on;
+gzip_proxied any;
+gzip_disable "MSIE [1-6]\.";
+EOF
+# Configure log_settings
+cat > ${log_settings} << EOF
+##
+# Nginx Logging Settings
+##
+
+access_log /var/log/nginx/access.log;
+error_log /var/log/nginx/error.log;
+EOF
+# Configure microcache_zone
+cat > ${microcache_zone} << EOF
+# Microcache Zone
+fastcgi_cache_path /var/cache/nginx/microcache levels=1:2 keys_zone=microcache:5M max_size=1G inactive=2h loader_threshold=2592000000 loader_sleep=1 loader_files=100000;
+EOF
+# Configure mime.types
+cat > ${mime.types} << EOF
+# Mime Types
+types {
+    text/html                               html htm shtml;
+    text/css                                css;
+    text/xml                                xml rss;
+    image/gif                               gif;
+    image/jpeg                              jpeg jpg;
+    application/x-javascript                js;
+    application/atom+xml                    atom;
+
+    text/mathml                             mml;
+    text/plain                              txt;
+    text/vnd.sun.j2me.app-descriptor        jad;
+    text/vnd.wap.wml                        wml;
+    text/x-component                        htc;
+
+    image/png                               png;
+    image/tiff                              tif tiff;
+    image/vnd.wap.wbmp                      wbmp;
+    image/x-icon                            ico;
+    image/x-jng                             jng;
+    image/x-ms-bmp                          bmp;
+    image/svg+xml                           svg svgz;
+
+    application/java-archive                jar war ear;
+    application/json                        json;
+    application/mac-binhex40                hqx;
+    application/msword                      doc;
+    application/pdf                         pdf;
+    application/postscript                  ps eps ai;
+    application/rtf                         rtf;
+    application/vnd.ms-excel                xls;
+    application/vnd.ms-powerpoint           ppt;
+    application/vnd.wap.wmlc                wmlc;
+    application/vnd.google-earth.kml+xml    kml;
+    application/vnd.google-earth.kmz        kmz;
+    application/x-7z-compressed             7z;
+    application/x-cocoa                     cco;
+    application/x-java-archive-diff         jardiff;
+    application/x-java-jnlp-file            jnlp;
+    application/x-makeself                  run;
+    application/x-perl                      pl pm;
+    application/x-pilot                     prc pdb;
+    application/x-rar-compressed            rar;
+    application/x-redhat-package-manager    rpm;
+    application/x-sea                       sea;
+    application/x-shockwave-flash           swf;
+    application/x-stuffit                   sit;
+    application/x-tcl                       tcl tk;
+    application/x-x509-ca-cert              der pem crt;
+    application/x-xpinstall                 xpi;
+    application/xhtml+xml                   xhtml;
+    application/zip                         zip;
+
+    application/octet-stream                bin exe dll;
+    application/octet-stream                deb;
+    application/octet-stream                dmg;
+    application/octet-stream                eot;
+    application/octet-stream                iso img;
+    application/octet-stream                msi msp msm;
+    application/ogg                         ogx;
+
+    audio/midi                              mid midi kar;
+    audio/mpeg                              mpga mpega mp2 mp3 m4a;
+    audio/ogg                               oga ogg spx;
+    audio/x-realaudio                       ra;
+    audio/webm                              weba;
+
+    video/3gpp                              3gpp 3gp;
+    video/mp4                               mp4;
+    video/mpeg                              mpeg mpg mpe;
+    video/ogg                               ogv;
+    video/quicktime                         mov;
+    video/webm                              webm;
+    video/x-flv                             flv;
+    video/x-mng                             mng;
+    video/x-ms-asf                          asx asf;
+    video/x-ms-wmv                          wmv;
+    video/x-msvideo                         avi;
+}
+EOF
 # Give permissions for log file
-#chmod 777 -R /var/log/nginx/error.log
+chmod 777 -R /var/log/nginx/error.log
 # Create phpinfo() file
-cat > ${local_path}/info.php << EOF
+cat > ${site_path}/info.php << EOF
 <?php
 phpinfo();
 ?>
@@ -292,6 +605,46 @@ EOF
 service mysql restart
 service nginx restart
 service php5-fpm restart
+# Install PhpMyAdmin
+echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect boolean true" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/dbconfig-install boolean true" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/mysql/admin-user string root" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/mysql/admin-pass password "${mysql_root_password} | debconf-set-selections
+echo "phpmyadmin phpmyadmin/mysql/app-pass password "${phpmyadmin_root_password} |debconf-set-selections
+echo "phpmyadmin phpmyadmin/app-password-confirm password "${phpmyadmin_root_password} | debconf-set-selections
+apt-get install phpmyadmin -y
+# Configure nginx.conf
+cat > ${phpmyadmin.conf} << EOF
+# PhpMyAdmin configuration
+
+location /phpmyadmin {
+       root /usr/share/;
+       index index.php index.html index.htm;
+       location ~ ^/phpmyadmin/(.+\\.php)\$ {
+               #try_files $uri =404;
+               root /usr/share/;
+               #fastcgi_pass 127.0.0.1:9000;
+              fastcgi_pass unix:/tmp/php5-fpm.sock;
+               fastcgi_index index.php;
+               fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+               include fastcgi_params;
+       }
+       location ~* ^/phpmyadmin/(.+\\.(jpg|jpeg|gif|css|png|js|ico|html|xml|txt))\$ {
+               root /usr/share/;
+       }
+}
+location /phpMyAdmin {
+       rewrite ^/* /phpmyadmin last;
+}
+EOF
+# Create a symbolic link between phpMyAdmin and website root directory
+ln -s /usr/share/phpmyadmin/ ${site_path}
+# Disable by default, as this will add to all VirtualHosts; instead, add the following to an Apache VirtualHost:
+#echo "Include /etc/phpmyadmin/apache.conf" >> /etc/apache2/apache2.conf
+# Restart services
+service mysql restart
+service nginx restart
+service php5-fpm restart
 #
 # Install Virtualbox and Vagrant
 #
@@ -300,20 +653,28 @@ echo ${green}................................ Installing VirtualBox and Vagrant 
 echo ${green}.................................................................................................${reset}
 sleep 5
 # IMPORTANT! Before using VirtualBox, make sure that virtualization is enabled in your BIOS settings
+# IMPORTANT! Make sure that VirtualBox not running right now
+# Uninstall VirtualBox if it was installed previously
+apt-get purge "^virtualbox-.*" -y
+apt-get update
+# Clean up
+apt-get autoremove -y | apt-get autoclean - y | apt-get clean -y
 # Add deb
-cat > /etc/apt/sources.list << EOF
-deb http://download.virtualbox.org/virtualbox/debian trusty contrib
+cat > /etc/apt/sources.list.d/oracle-vbox.list << EOF
+deb http://download.virtualbox.org/virtualbox/debian trusty contrib  
+# deb-src http://download.virtualbox.org/virtualbox/debian trusty contrib
 EOF
-# Now install PGP key
-wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add --
+# Now, download and register the ORACLE public key
+wget -q -O - https://www.virtualbox.org/download/oracle_vbox.asc | sudo apt-key add -  
 # Update and install virtualbox
 apt-get update
-apt-get install virtualbox-5.0 -y
+dpkg --configure -a
+apt-get install dkms virtualbox-5.0 -y
 # Install Extension Pack for VirtualBox
 # You can check latest extension pack version here - https://www.virtualbox.org/wiki/Downloads
-#ext_pack="Oracle_VM_VirtualBox_Extension_Pack-5.0.12-104815.vbox-extpack"
-#wget http://download.virtualbox.org/virtualbox/5.0.12/${ext_pack}
-#echo ${root_pass} | VBoxManage extpack install ${ext_pack}
+ext_pack="Oracle_VM_VirtualBox_Extension_Pack-5.0.12-104815.vbox-extpack"
+wget http://download.virtualbox.org/virtualbox/5.0.12/${ext_pack}
+echo ${root_pass} | VBoxManage extpack install ${ext_pack}
 # install Vagrant
 # Get deb,unpack it and remove after installing
 wget https://releases.hashicorp.com/vagrant/1.8.1/vagrant_1.8.1_x86_64.deb
