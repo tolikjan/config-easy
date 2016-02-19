f4fb17fe254f09b58a09d93f3e0468863f6c73a9
tolikjan <tolikjan@gmail.com>
2016-02-19 12:16:39 +0200

    fixed versions for virtualbox, selenium, chrome driver. add phpmyadmin instalstion script

--- config-ubuntu.sh
+++ config-ubuntu.sh
@@ -10,70 +10,51 @@ root_pass="osboxes.org"
 red=`tput setaf 1`
 green=`tput setaf 2`
 reset=`tput sgr0`
-
-sleep 5
 #
-# Update & Upgrade
-#
+### Update & Upgrade system
 echo ${green}.................................................................................................${reset}
-echo ${green}................................ Update and Upgrade the system ..................................${reset}
+echo ${green}................................. Update and Upgrade the system .................................${reset}
 echo ${green}.................................................................................................${reset}
-sleep 5
 apt-get update && apt-get upgrade -y
 # disable guest session
 echo "allow-guest=false" >> /usr/share/lightdm/lightdm.conf.d/50-ubuntu.conf
+# configure system to allow more performance
+echo "vm.swappiness=10" >> /etc/sysctl.conf
 #
-# Install Chrome
-#
+### Install Google Chrome https://www.google.com/chrome/
 echo ${green}.................................................................................................${reset}
 echo ${green}................................... Installing Google Chrome ....................................${reset}
 echo ${green}.................................................................................................${reset}
-sleep 5
-# Another way which occur errors after Updating System
 # TODO: Need check whether chrome driver works with selenium
-# 
-#wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-#sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-#sudo apt-get update
-#sudo apt-get install google-chrome-stable -y
-#
-# Get deb,unpack it and remove after installing
+# Get deb, unpack it and remove after installing
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg -i ./google-chrome*.deb
 apt-get install -f -y
 rm -rf google-chrome*.deb
 #
-# Install Flash player for Firefox
-#
+### Install Flash player for Firefox
 echo ${green}.................................................................................................${reset}
 echo ${green}.............................. Installing Flash player for Firefox ..............................${reset}
 echo ${green}.................................................................................................${reset}
-sleep 5
 apt-get install flashplugin-installer
 #
-# Install Tweak Tools for Ubuntu additional settings
+### Install Tweak Tools for Ubuntu additional settings
+#echo ${green}.................................................................................................${reset}
+#echo ${green}.................................... Installing Tweak Tools .....................................${reset}
+#echo ${green}.................................................................................................${reset}
+#apt-get install unity-tweak-tool
 #
+### Install utilites for archive manager with 7z and rar support
 echo ${green}.................................................................................................${reset}
-echo ${green}................................... Installing Tweak Tools ......................................${reset}
+echo ${green}.................................... Installing 7z and Unrar ....................................${reset}
 echo ${green}.................................................................................................${reset}
-sleep 5
-apt-get install unity-tweak-tool
-#
-# Install utilites for archive manager with 7z and rar support
-#
-echo ${green}.................................................................................................${reset}
-echo ${green}.................................. Installing 7z and Unrar ......................................${reset}
-echo ${green}.................................................................................................${reset}
-sleep 5
 apt-get install p7zip-full -y
 sudo apt-get install unrar -y
 #
-# Install Skype
-#
+### Install Skype http://www.skype.com/
 echo ${green}.................................................................................................${reset}
-echo ${green}..................................... Installing Skype ..........................................${reset}
+echo ${green}........................................ Installing Skype .......................................${reset}
 echo ${green}.................................................................................................${reset}
-sleep 5
 # Install dependencies for Skype
 apt-get install -y sni-qt:i386 libdbusmenu-qt2:i386 libqt4-dbus:i386 libxss1:i386
 apt-get install -y libgtk2.0-0:i386 gtk2-engines:i386 libgconf-2-4:i386
@@ -84,123 +65,108 @@ apt-get install skype -y
 # Install sound plugins for fixing problems with sound for Ubuntu
 apt-get install libasound2-plugins:i386 -y
 #
-# Install Tor Browser
-#
+### Install Tor Browser https://www.torproject.org/
 echo ${green}.................................................................................................${reset}
-echo ${green}................................... Installing Tor Browser ......................................${reset}
+echo ${green}.................................... Installing Tor Browser .....................................${reset}
 echo ${green}.................................................................................................${reset}
-sleep 5
 echo -ne '\n' | add-apt-repository ppa:webupd8team/tor-browser
 apt-get update
 apt-get install tor-browser -y
 #
-# Install Tox http://utox.org/
-#
+### Install Tox Messenger http://utox.org/
 echo ${green}.................................................................................................${reset}
-echo ${green}...................................... Installing uTox ..........................................${reset}
+echo ${green}........................................ Installing uTox ........................................${reset}
 echo ${green}.................................................................................................${reset}
-sleep 5
 echo -ne '\n' | add-apt-repository ppa:v-2e/tox
 apt-get update
 apt-get install utox -y
 #
-# Install Telegram messanger
-#
+### Install Telegram messenger https://telegram.org/
 echo ${green}.................................................................................................${reset}
-echo ${green}..................................... Installing Telegram .......................................${reset}
+echo ${green}...................................... Installing Telegram ......................................${reset}
 echo ${green}.................................................................................................${reset}
-sleep 5
 echo -ne '\n' | add-apt-repository ppa:atareao/telegram
 apt-get update
 apt-get install telegram -y
 #
-# Install Shutter
-#
+### Install Shutter http://shutter-project.org/
 echo ${green}.................................................................................................${reset}
-echo ${green}..................................... Installing Shutter ........................................${reset}
+echo ${green}...................................... Installing Shutter .......................................${reset}
 echo ${green}.................................................................................................${reset}
-sleep 5
 echo -ne '\n' | add-apt-repository ppa:shutter/ppa
 apt-get install shutter -y
 #
-# Install SSH
-#
+### Install SSH
 echo ${green}.................................................................................................${reset}
 echo ${green}..................................... Installing SSH Server .....................................${reset}
 echo ${green}.................................................................................................${reset}
-sleep 5
 apt-get update
 apt-get install openssh-client -y
 apt-get install openssh-server -y
 mkdir ~/.ssh
 chmod 777 -R ~/.ssh/
 #
-# Install Git
-#
+### Install Git
 echo ${green}.................................................................................................${reset}
 echo ${green}......................................... Installing Git ........................................${reset}
 echo ${green}.................................................................................................${reset}
-sleep 5
 apt-get update
 apt-get install git -y
 apt-get install tig -y
 #
-# Install Composer
-#
+### Install Composer
 echo ${green}.................................................................................................${reset}
-echo ${green}....................................... Installing Composer .....................................${reset}
+echo ${green}...................................... Installing Composer ......................................${reset}
 echo ${green}.................................................................................................${reset}
-sleep 5
 apt-get update
 apt-get install php5-curl -y
 apt-get install curl php5-cli git -y
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 chmod 777 -R ~/.composer/
-# Install codesnifer for phpStorm
+echo ${green}.................................................................................................${reset}
+echo ${green}.................................... Installing Codesniffer .....................................${reset}
+echo ${green}.................................................................................................${reset}
+#
+### Install codesniffer for phpStorm
+# TODO: Check the codesnifer instalation
 #composer global require drupal/coder
 #export PATH="$PATH:$HOME/.composer/vendor/bin"
 #phpcs --config-set installed_paths ~/.composer/vendor/drupal/coder/coder_sniffer
 #
-# Install Selenium Server
-#
+### Install Selenium Server http://www.seleniumhq.org/
 echo ${green}.................................................................................................${reset}
 echo ${green}.................................. Installing Selenium Server ...................................${reset}
 echo ${green}.................................................................................................${reset}
-sleep 5
 echo -ne '\n' | add-apt-repository ppa:webupd8team/java
 apt-get update
 # Create folder for Selenium
 mkdir ~/selenium
 chmod 777 -R selenium/
 cd ~/selenium
-# Install xvfb - display server wich implementing the X11 display server protocol
+# Install xvfb - display server which implementing the X11 display server protocol
 apt-get install xvfb -y
-# Get Selenium and install headless Java runtime
-wget http://selenium-release.storage.googleapis.com/2.48/selenium-server-standalone-2.48.2.jar
+# Get Selenium and install headless Java runtime (you should check newest release on http://www.seleniumhq.org/download/)
+wget http://selenium-release.storage.googleapis.com/2.52/selenium-server-standalone-2.52.0.jar
 apt-get install openjdk-7-jre-headless -y
 # Install headless GUI for firefox. Xvfb is a display server that performs graphical operations in memory
 #apt-get install xvfb -y
 # Starting up Selenium server
-#DISPLAY=:1 xvfb-run java -jar ~/selenium/selenium-server-standalone-2.48.2.jar
-wget -N http://chromedriver.storage.googleapis.com/2.20/chromedriver_linux64.zip
+#DISPLAY=:1 xvfb-run java -jar ~/selenium/selenium-server-standalone-2.52.0.jar
+wget -N http://chromedriver.storage.googleapis.com/2.21/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
 rm -rf chromedriver_linux64.zip
 chmod 777 -R ~/selenium/
-###
-# Install LEMP (nginx + MySQL + PHPMyAdmin) and configure it
-###
+#
+### Install LEMP (nginx + MySQL + PHPMyAdmin) and configure it
 echo ${green}.................................................................................................${reset}
-echo ${green}.................... Installing and Configuring LEMP - Linux + nginx + MySQL ....................${reset}
+echo ${green}.................... Installing and Configuring LEMP — Linux + nginx + MySQL ....................${reset}
 echo ${green}.................................................................................................${reset}
 # Uninstall/Clear possible extra programs
 apt-get purge -y apache2* php5* mysql*
 dpkg -l | grep apache*
 dpkg -l | grep php5*
 dpkg -l | grep mysql*
-###
 # Set Up variables
-###
-# TODO: Do something with this fucking web server
 # php.ini path
 php_config_file1="/etc/php5/fpm/php.ini"
 # site folder path
@@ -219,9 +185,7 @@ phpmyadmin.conf="/etc/nginx/phpmyadmin.conf"
 mysql_config_file="/etc/mysql/my.cnf"
 mysql_root_user="root"
 mysql_root_password="root"
-###
 # Install nginx
-###
 echo -ne '\n' | add-apt-repository ppa:nginx/stable
 apt-get update
 apt-get upgrade -y
@@ -270,20 +234,23 @@ server {
 EOF
 # Add site name to /etc/hosts
 echo "127.0.0.1       ${server_name}" >> /etc/hosts
-###
-# Install mysql-server
-###
-# Set password for root account
+# Install mysql-server and phpmyadmin
+apt-get update
 echo "mysql-server mysql-server/root_password password $mysql_root_password" | debconf-set-selections
 echo "mysql-server mysql-server/root_password_again password $mysql_root_password" | debconf-set-selections
-apt-get install mysql-server php5-mysql -y
-###
+echo "phpmyadmin phpmyadmin/dbconfig-install boolean true" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/app-password-confirm password $mysql_root_password" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/mysql/admin-pass password $mysql_root_password" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/mysql/app-pass password $mysql_root_password" | debconf-set-selections
+echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect none" | debconf-set-selections
+apt-get install mysql-server php5-mysql phpmyadmin -y
+ln -s /usr/share/phpmyadmin /usr/share/nginx/html
+# Enable mycrypt and restart service
+php5enmod mcrypt
+service php5-fpm restart
 # Install PHP
-###
 apt-get install php5 php5-fpm php5-mysql php5-cli php5-curl php5-gd php5-mcrypt php5-xdebug -y
-###
 # php.ini error reporting configuring
-###
 for ini1 in $(find /etc -name 'php.ini')
 do
     sed -i 's/^error_reporting = E_ALL & ~E_DEPRECATED & ~E_STRICT/error_reporting = E_ALL/' ${ini1}
@@ -295,9 +262,7 @@ do
     sed -i 's/^post_max_size = 8M/post_max_size = 200M/' ${ini1}
     sed -i 's/^upload_max_filesize = 2M/upload_max_filesize = 200M/' ${ini1}
 done
-###
 # php.ini xdebug configuring
-###
 for ini in $(find /etc -name 'php.ini')
 do
     xdebug=$(find / -name "xdebug.so" 2> /dev/null)
@@ -315,10 +280,8 @@ do
     echo 'xdebug.var_display_max_depth = 5' >> ${ini}
     echo 'xdebug.var_display_max_children = 256' >> ${ini}
     echo 'xdebug.var_display_max_data = 1024' >> ${ini}
-done    
-###
+done
 # Change configuration www.conf
-###
 sed -i 's/^listen =  127.0.0.1:9000/listen = /var/run/php5-fpm.sock/' ${www_conf}
 # Create phpinfo() file
 cat > ${site_path}/info.php << EOF
@@ -328,13 +291,11 @@ EOF
 service mysql restart
 service nginx restart
 service php5-fpm restart
-###
-# Install Virtualbox and Vagrant
-###
+#
+### Install VirtualBox and Vagrant
 echo ${green}.................................................................................................${reset}
-echo ${green}................................ Installing VirtualBox and Vagrant ..............................${reset}
+echo ${green}............................... Installing VirtualBox and Vagrant ...............................${reset}
 echo ${green}.................................................................................................${reset}
-sleep 5
 # IMPORTANT! Before using VirtualBox, make sure that virtualization is enabled in your BIOS settings
 # IMPORTANT! Make sure that VirtualBox not running right now
 # Uninstall VirtualBox if it was installed previously
@@ -352,20 +313,48 @@ wget -q -O - https://www.virtualbox.org/download/oracle_vbox.asc | sudo apt-key 
 # Update and install virtualbox
 apt-get update
 dpkg --configure -a
-apt-get install dkms virtualbox-5.0 -y
+apt-get install dkms virtualbox-5.0.14 -y
 # Install Extension Pack for VirtualBox
 # You can check latest extension pack version here - https://www.virtualbox.org/wiki/Downloads
 ext_pack="Oracle_VM_VirtualBox_Extension_Pack-5.0.14.vbox-extpack"
 wget http://download.virtualbox.org/virtualbox/5.0.14/${ext_pack}
 echo ${root_pass} | VBoxManage extpack install ${ext_pack}
 rm -rf ${ext_pack}
-# install Vagrant
+# Install Vagrant
 # Get deb,unpack it and remove after installing
 wget https://releases.hashicorp.com/vagrant/1.8.1/vagrant_1.8.1_x86_64.deb
 dpkg -i vagrant*.deb
 apt-get install vagrant -y
 apt-get install -f -y
 rm -rf vagrant*.deb
+# Install SublimeText 3 https://www.sublimetext.com/3
+# TODO: Licence code here - https://gist.github.com/J2TeaM/9f24a57d5832e475fc4d
+echo ${green}.................................................................................................${reset}
+echo ${green}.................................... Installing SublimeText 3 ...................................${reset}
+echo ${green}.................................................................................................${reset}
+sleep 5
+echo -ne '\n' | add-apt-repository ppa:webupd8team/sublime-text-3
+apt-get update
+apt-get install sublime-text-installer -y
+# Install PhpStorm 10 https://www.jetbrains.com/phpstorm/download/
+# Licence code here - https://бэкдор.рф/phpstorm-7-8-9-10-product-key/
+echo ${green}.................................................................................................${reset}
+echo ${green}............................. Installing and Configuring PHPStopm 10 ............................${reset}
+echo ${green}.................................................................................................${reset}
+wget http://download-cf.jetbrains.com/webide/PhpStorm-10.0.3.tar.gz
+tar -xvf PhpStorm-10.0.3.tar.gz
+# NOTE: For complete installation, you should execute two commands below from Terminal after finishing
+#cd PhpStorm-*/bin/
+#./phpstorm.sh || TRUE
+# Install HipChat https://www.hipchat.com/
+echo ${green}.................................................................................................${reset}
+echo ${green}...................................... Installing HipChat .......................................${reset}
+echo ${green}.................................................................................................${reset}
+echo osboxes.org | sudo su
+echo "deb http://downloads.hipchat.com/linux/apt stable main" > /etc/apt/sources.list.d/atlassian-hipchat.list
+wget -O - https://www.hipchat.com/keys/hipchat-linux.key | apt-key add -
+apt-get update
+apt-get install hipchat
 echo ${green}.................................................................................................${reset}
 echo ${green}............................................. DONE ..............................................${reset}
 echo ${green}.................................................................................................${reset}
