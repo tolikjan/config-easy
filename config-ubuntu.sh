4490cd6dccd5a5d8e404c4f55df42a5de94049cb
tolikjan <tolikjan@gmail.com>
2016-03-23 11:25:29 +0200

    hot-fix in description

--- config-ubuntu.sh
+++ config-ubuntu.sh
@@ -4,7 +4,7 @@
 # Tested on Ubuntu 14.04.3 64x
 #
 # For testing I used vagrant-box
-# http://sourceforge.net/projects/osboxes/files/vms/vbox/Ubuntu/14.04/14.04.3/Ubuntu_14.04.3-64bit.7z/download
+# https://sourceforge.net/projects/osboxes/files/vms/vbox/Ubuntu/14.04/14.04.4/Ubuntu_14.04.4-64bit.7z/download
 ROOT_PASS="osboxes.org"
 #
 # type your own password below which should be correct for you system
