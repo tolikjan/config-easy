5e1f221ace06ac48f64d1e21a1ce491701b6538f
tolikjan <tolikjan@gmail.com>
2016-03-25 19:26:09 +0200

    added hot-fix

--- main-script.sh
+++ main-script.sh
@@ -80,7 +80,7 @@ hipChat() {
 }
 
 # List of tools where your can choose all preferred programs
-whiptail --title "Config Ubuntu Script" --checklist  --output-fd fd "Use <Space> to choose tools which you want to install:" 30 58 23 \
+dialog --title "Config Ubuntu Script" --checklist  --output-fd fd "Use <Space> to choose tools which you want to install:" 30 58 23 \
 "UpdateUpgrade" "" on \
 "GoogleChrome" "" on \
 "xclip" "" off \
